3c8b67d1c9a03aa189a471927442964f8da337fe
gjchen <gjchen.tw@gmail.com>
2021-10-12 23:28:18 +0800

    debugger working in vscode

--- example/main.c
+++ example/main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#define DEBUG_HV
 #ifdef DEBUG_HV
 #include <json-c/json.h>
-#include "hediet-visualizer.h"
+#include "../include/hediet-visualizer.h"
 #endif
 
 int main(void)
 {
 #ifdef DEBUG_HV
     json_object *debugHV;
+    const char *strDebugHV;
 #endif
 
 #ifdef DEBUG_HV
     debugHV = get_hv_wrapper();
+    strDebugHV = json_object_to_json_string(debugHV);
+    printf("%s", strDebugHV);
 #endif
     return 0;
 }
+
+///usr/bin/gcc -ljson-c -Lbuild -lhediet-visualizer -g /home/gjchen/projects/lib-hediet-visualizer/example/main.c -o /home/gjchen/projects/lib-hediet-visualizer/example/main
--- include/hediet-visualizer.h
+++ include/hediet-visualizer.h
@@ -1,7 +1,6 @@
-#ifndef _HEDEIT_VISUALIZER_H_
-#define _HEDEIT_VISUALIZER_H_
+#ifndef _HEDIET_VISUALIZER_H_
+#define _HEDIET_VISUALIZER_H_
 
-char *get_hv_wrapper(void);
+extern json_object * get_hv_wrapper(void);
 
 #endif
-
